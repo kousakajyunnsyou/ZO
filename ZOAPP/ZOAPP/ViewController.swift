a2a7abbfbea580891423ff2d4f1b528a5d96a90d
kousakajyunnsyou <kousakajyunnsyou@gmail.com>
2021-04-16 17:02:14 +0800

    初始化

---
+++ ZOAPP/ZOAPP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ZOAPP
+//
+//  Created by yons on 2021/4/16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
