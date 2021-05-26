84e2ce395d2b1dc23b4106ad99b8a3fc4414f341
kousakajyunnsyou <kousakajyunnsyou@gmail.com>
2021-05-26 18:18:59 +0800

    2021/05/26

---
+++ ZO/Tools/NetWorkTools.swift
@@ -0,0 +1,33 @@
+//
+//  NetWorkTools.swift
+//  ZO
+//
+//  Created by yons on 2021/5/26.
+//  简单的网络请求封装
+
+import UIKit
+import Alamofire
+
+enum RequestMethod {
+    case GET
+    case POST
+}
+
+public typealias finishCallback = (_ resp : AnyObject) -> ()
+
+class NetWorkTools: NSObject {
+
+    class func requestForData(method : RequestMethod, url : String ,paras: [String: String]? = nil, callback:@escaping finishCallback) {
+       
+        let httpMethod = method == RequestMethod.GET ? HTTPMethod.get : HTTPMethod.post
+        
+        AF.request(url ,method: httpMethod, parameters: paras).responseJSON { (result) in
+            guard  let resp = result.value else {
+                print(result.error as Any)
+                return
+            }
+            
+            callback(resp as AnyObject)
+        }
+    }
+}
