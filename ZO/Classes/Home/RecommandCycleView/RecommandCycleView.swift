d5d49e3c72b074c5d4f0517d4c273dd0fadef363
dousei hann <>
2022-02-17 15:12:04 +0800

    无限轮播展示完成

--- ZO/Classes/Home/RecommandCycleView/RecommandCycleView.swift
+++ ZO/Classes/Home/RecommandCycleView/RecommandCycleView.swift
@@ -20,6 +20,9 @@ class RecommandCycleView: UIView {
         didSet {
             cycleView.reloadData()
             pageControl.numberOfPages = cycleModels?.count ?? 0
+            
+            let indexPath = IndexPath(item: (cycleModels?.count ?? 0) * 10, section: 0)
+            cycleView.scrollToItem(at: indexPath, at: .left, animated: false)
         }
     }
     
@@ -45,17 +48,22 @@ extension RecommandCycleView {
 // MARK: - 遵守UICollectionView的数据源协议
 extension RecommandCycleView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cycleModels?.count ?? 0
+        return (cycleModels?.count ?? 0) * 10000
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KCycleCellID, for: indexPath) as! CycleCollectionViewCell
-        cell.cycleRoom = self.cycleModels?[indexPath.item]
+        cell.cycleRoom = self.cycleModels?[indexPath.item % (cycleModels?.count ?? 1)]
         return cell
     }
 }
 
 // MARK: - 遵守UICollectionViewDelegate
 extension RecommandCycleView: UICollectionViewDelegate {
-    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //增加宽度的一半，则在翻到一般时，pageControl即可发生动作
+        let offsetX = scrollView.contentOffset.x + scrollView.bounds.width * 0.5
+        
+        pageControl.currentPage = Int(offsetX / scrollView.bounds.width) % (cycleModels?.count ?? 1)
+    }
 }
